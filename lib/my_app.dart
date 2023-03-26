66e1a5a5cc97d593d13aa2c09c2631406b9d7e96
Rafael Tavares <rafael.tavares@anpix.com.br>
2023-03-26 11:52:19 -0300

    Video 04 - Primeiro Aplicativo de Cripto Moedas
    https://www.youtube.com/watch?v=Hg-BE-BCtcU&list=PL_wKlpKIC9vWCRIgMvH8pbRmX8XVouRv1&index=4

---
+++ lib/my_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Crypto',
+      //debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
