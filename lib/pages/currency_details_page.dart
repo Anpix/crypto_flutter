8a3a1eca8c80148afb5557ffea2fe2b08ca397fd
Rafael Tavares <rafael.tavares@anpix.com.br>
2023-03-26 14:48:33 -0300

    Video 13 - Como customizar um formulário e botão
    https://www.youtube.com/watch?v=TBhTFXe8f3s&list=PL_wKlpKIC9vWCRIgMvH8pbRmX8XVouRv1&index=13

--- lib/pages/currency_details_page.dart
+++ lib/pages/currency_details_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 import '../models/currency.dart';
 
@@ -8,28 +10,133 @@ class CurrencyDetailsPage extends StatefulWidget {
   Currency currency;
 
   @override
-  _CurrencyDetailsPage createState() => _CurrencyDetailsPage();
+  State<CurrencyDetailsPage> createState() => _CurrencyDetailsPage();
 }
 
 class _CurrencyDetailsPage extends State<CurrencyDetailsPage> {
+  NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+  final _form = GlobalKey<FormState>();
+  final _value = TextEditingController();
+  double ammount = 0;
+
+  reserve() {
+    if (_form.currentState!.validate()) {
+      //save
+
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('The money was reserved successfully!')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.currency.name),
       ),
-      body: Column(
-        children: [
-          const Divider(),
-          Row(
-            children: [
-              SizedBox(
-                width: 50,
-                child: Image.asset(widget.currency.icon),
-              )
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            const Divider(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    child: Image.asset(widget.currency.icon),
+                  ),
+                  Container(width: 10),
+                  Text(
+                    real.format(widget.currency.price),
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: -1,
+                      color: Colors.grey[800],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            (ammount > 0)
+                ? SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 24),
+                      padding: const EdgeInsets.all(12),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(color: Colors.teal.withOpacity(0.05)),
+                      child: Text(
+                        '$ammount ${widget.currency.acronym}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(
+                    margin: const EdgeInsets.only(bottom: 71),
+                  ),
+            Form(
+              key: _form,
+              child: TextFormField(
+                controller: _value,
+                style: const TextStyle(fontSize: 22),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Value',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  suffix: Text(
+                    'reais',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Inform the value';
+                  } else if (double.parse(value) < 50) {
+                    return 'Compra mínima de R\$ 50,00';
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    ammount = (value.isEmpty) ? 0 : double.parse(value) / widget.currency.price;
+                  });
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: const EdgeInsets.only(top: 24),
+              child: ElevatedButton(
+                  onPressed: reserve,
+                  child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.check),
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: Text(
+                          'Reserve',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
           ],
-          )
-        ],
+        ),
       ),
     );
   }
